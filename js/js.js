d83cdd2e3eca77d0256624af54e9db7405270f6f
Alcotras71 <60799652+Alcotras71@users.noreply.github.com>
2020-03-20 17:14:20 +0300

    Add files via upload
    add new page. refresh css files. add some in js.

--- js/js.js
+++ js/js.js
@@ -11,3 +11,40 @@ jQuery(function($){
 	});
 
 });
+
+
+let select = function () {
+	let selectHeader = document.querySelectorAll('.select__header');
+	let selectItem = document.querySelectorAll('.select__item');
+
+	selectHeader.forEach( item=> {
+		item.addEventListener('click', selectToggle)
+	});
+
+	selectItem.forEach( item=> {
+		item.addEventListener('click', selectChoose)
+	});
+
+	function selectToggle() {
+		this.parentElement.classList.toggle('is-active');
+	}
+
+	$(".select__item").click(function(e) {
+		e.preventDefault();
+		$(".select__item").removeClass('hidden');
+		$(this).addClass('hidden');
+})
+
+	function selectChoose() {
+		let text = this.innerText,
+			 select = this.closest('.select'),
+			 currentText = this.closest('.select').querySelector('.select__current');
+		currentText.innerText = text;
+		select.classList.remove('is-active');
+	}
+};
+
+select();
+
+let current = $('.select__current').text();
+	$('#current_worker').val(current);
